87d1eb317d1be450bbf855549337b581ad0eb8b8
Savior135 <srivastava.amankumar03@gmail.com>
2019-06-06 21:44:54 +0530

    first commit

---
+++ MLG.py
@@ -0,0 +1,29 @@
+
+# coding: utf-8
+
+# In[2]:
+
+
+print("WELCOME,LET'S GO MAD WITH MAD LIB GENERATOR")
+a=input("choose a book character: ")
+b=input("choose a horrific adjective")
+c=input("choose a verb ending with -ing: ")
+d=input("choose a verb ending with -ing: ")
+e=input("choose another character: ")
+f=input("choose a place name: ")
+g=input("choose your favorite book: ")
+h=input("your librarian name")
+i=input("exclamation")
+j=input("number between 1-12")
+print("A CRAZZY NIGHT AT THE LIBRARY")
+
+print('''One night something really '''+str(b)+ ''' hapened at the library. The characters in the stories started '''+str(c)+ ''' from their books! '''
+
++str(a)+ ''' climbed into Charlotte's Web and started ''' +str(d)+ ''' with Wilbur the Pig! ''' +str(e)+ ''' wandered into an encyclopedia and ended up lost in ''' +str(f)+ '''  !
+
+The craziest part was when Emilia wandered into and said, “Rocky, I don't think we're in Chicago any more.”
+
+Then '''+str(h)+ '''  walked in and said, “ '''+str(i)+''' ! Everybody back in place!”
+
+The characters around the room. Just in time! The students in grade ''' +str(j)+ ''' came in to get books for their reports, but it was safe. All the characters were back in the books where they belonged.''')
+
